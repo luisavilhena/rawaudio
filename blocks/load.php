1fdb409ba7be1caa770dd84d62869dc37559483d
luisavilhena <luisavilhena@gmail.com>
2022-01-28 19:31:18 -0300

    apaguei muita coisa

--- blocks/load.php
+++ blocks/load.php
@@ -2,13 +2,5 @@
 	require('carousel.php');
 	require('placard.php');
 	require('list-title-image.php');
-	
-	require('rich-text.php');
-	require('image-columns.php');
-	require('one-image.php');
-	require('text-box.php');
 	require('social-media.php');
-	require('columns-image-text-box.php');
-	require('columns-image-text-box-img-sm.php');
-	require('image-sizes.php');
 ?>
